9781990259bb33289d47b33c0b72e5fcded5495e
wpmuguru <wpmuguru@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-28 14:34:39 +0000

    bump trunk version
    git-svn-id: https://plugins.svn.wordpress.org/post-gallery-widget/trunk@316257 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post-gallery.php
+++ post-gallery.php
@@ -4,7 +4,7 @@ Plugin Name: Rotating Post Gallery
 Plugin URI: http://wpmututorials.com/plugins/post-gallery-widget/
 Description: A Rotating Gallery Widget using a custom post type to create Gallery Posts.
 Author: Ron Rennick
-Version: 0.2.1
+Version: 0.2.1.1
 Author URI: http://ronandandrea.com/
 
 This plugin is a collaboration project with contributions from the CUNY Acedemic Commons (http://dev.commons.gc.cuny.edu/)
